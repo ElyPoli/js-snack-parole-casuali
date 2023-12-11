b447d178b89b3c0409c583e294784e9216797992
ElyPoli <itsos.poli@gmail.com>
2023-12-11 16:35:29 +0100

    print n words generated via api and called axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+// Dichiarazioni
+const wordsToGenerate = 10;
+const arrayRandomWords = [];
+
+// Funzione per la chiamata axios che genera una parola casuale
+function generateRandomWord() {
+  let url = "https://flynn.boolean.careers/exercises/api/random/word";
+
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data.response);
+      arrayRandomWords[arrayRandomWords.length] = response.data.response;
+
+      // Verifico se ho generato tutte le parole randomiche, se no richiamo nuovamente la funzione
+      if (arrayRandomWords.length < wordsToGenerate) {
+        generateRandomWord();
+      } else {
+        console.log("Parole generate:", arrayRandomWords);
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+// Richiamo la funzione che genera una parola casuale
+generateRandomWord();
